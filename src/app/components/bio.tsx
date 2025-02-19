da961641631514064734ca69290ebaed6d63261b
Fred <frederik.koenig@posteo.de>
2025-02-19 18:53:08 +0100

    internationalization bio

--- src/app/components/bio.tsx
+++ src/app/components/bio.tsx
@@ -1,14 +1,16 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 import { FaMapMarkerAlt, FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 
 export function Bio() {
+  const t = useTranslations("Bio");
   return (
     <section className="bg-gradient-to-br from-bluedark to-bluewhite tracking-wider">
       <div className=" flex justify-center bg-bluepull w-full " id="bio">
         <h1 className="tracking-wide text-xl md:text-3xl font-bold text-white py-4 md:py-6 ">
-          BIO
+          {t("TitleBio")}
         </h1>
       </div>
       <div className="container mx-auto p-4 text-white">
@@ -25,9 +27,9 @@ export function Bio() {
           <div className="md:w-2/3 md:pl-12">
             <h1 className="text-4xl font-bold mb-2">Frederik König</h1>
             <p className="text-2xl mb-4 flex items-center">
-              <FaMapMarkerAlt className="mr-2" /> Leipzig, Germany
+              <FaMapMarkerAlt className="mr-2" /> {t("Location")}
             </p>
-            <p className="text-2xl mb-4">Web Developer | Translator </p>
+            <p className="text-2xl mb-4">{t("Prof")}</p>
             <div className="flex space-x-4">
               <Link
                 href="https://github.com/Fredmpk"
@@ -54,58 +56,46 @@ export function Bio() {
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="bg-transgreen bg-opacity-20 rounded-lg p-6 backdrop-filter backdrop-blur-lg">
-            <h2 className="text-2xl font-semibold mb-4">
-              Professional Experiences
-            </h2>
+            <h2 className="text-2xl font-semibold mb-4">{t("Exp")}</h2>
             <ul className="space-y-2 text-xl">
-              <li>🌐 Freelance Translator since 2022</li>
-              <li className="ml-7 text-lg italic">
-                French - German / English-German
-              </li>
-              <li>🛠️ Freelance Craftsman since 2016</li>
+              <li>{t("FreeLTr")}</li>
+              <li className="ml-7 text-lg italic">{t("Languages")}</li>
+              <li>{t("FreeLCr")}</li>
             </ul>
           </div>
 
           <div className="bg-green bg-opacity-10 rounded-lg p-6 backdrop-filter backdrop-blur-lg">
-            <h2 className="text-2xl font-semibold mb-4">Education</h2>
+            <h2 className="text-2xl font-semibold mb-4">{t("Education")}</h2>
             <ul className="space-y-2 text-xl">
-              <li>🚀 Frontend Developer - Devhaus Leipzig | 2024</li>
+              <li>{t("Devhaus")}</li>
               <li>
-                🎓 M.A. Translatology - Universität Leipzig | 2018-2022
-                <p className="text-lg mt-1 ml-7 italic">
-                  Fields: Law, Medicine, Business, Tourism and Linguistics
-                </p>
+                {t("Translatology")}
+                <p className="text-lg mt-1 ml-7 italic">{t("FieldsTr")}</p>
               </li>
+              {t("Afr")}
               <li>
-                🎓 B.A. African Studies - Universität Leipzig | 2013-2018
-                <p className="text-lg mt-1 ml-7 italic">
-                  Fields: Haussa, Language, Culture, Media, Social Structures,
-                  Politics and Economy
-                </p>
+                <p className="text-lg mt-1 ml-7 italic">{t("FieldsAfr")}</p>
               </li>
             </ul>
           </div>
 
           <div className="bg-footerdarkgreen bg-opacity-10 rounded-lg p-6 backdrop-filter backdrop-blur-lg md:col-span-2">
-            <h2 className="text-2xl font-semibold mb-4">
-              International Experiences
-            </h2>
+            <h2 className="text-2xl font-semibold mb-4">{t("IntExp")}</h2>
             <ul className="space-y-2 text-xl">
               <li>
                 <span role="img" aria-label="Niger Flag">
                   🇳🇪
                 </span>{" "}
-                Semester Abroad - Université Abdou Moumouni, Niger | 2015/2016
-                <p className="text-lg mt-1 ml-7 italic">
-                  Sociology and Haussa course
-                </p>
+                {t("Niamey")}
+                <p className="text-lg mt-1 ml-7 italic">{t("Soc")}</p>
               </li>
               <li>
                 <span role="img" aria-label="Togo Flag">
                   🇹🇬
                 </span>{" "}
-                Volunteer work - Collège Protestant, Kpalimé, Togo | 2012/2013
+                {t("Togo")}
               </li>
+              <p className="text-lg mt-1 ml-7 italic">{t("TogoWork")}</p>
             </ul>
           </div>
         </div>
